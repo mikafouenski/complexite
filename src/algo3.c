125aee06781a2443c45d079909a4d51d8ec8fdb6
Mickaël Bernardini <contact@mickael-bernardini.fr>
2016-09-20 11:18:22 +0200

    Make algo3

--- src/algo3.c
+++ src/algo3.c
@@ -1,44 +1,52 @@
+#include <stdio.h>
+#include <limits.h>
 #include "utils.c"
 
-int seqSommeMax(int *t, int start, int end, int *start_max, int *end_max)
-{
-    int somme;
-    int sommeMax = t[0];
-
-    for( int i=start; i< end; ++i) {
-        somme = 0;
-        for(int j=i; j<end; ++j) {
-            somme += t[j];
-            if(somme > sommeMax) {
-                *start_max = i;
-                *end_max = j;
-                sommeMax = somme;
-            }
-        }
-    }
-
-    return sommeMax;
+int max2(int a, int b) { 
+    return (a > b)? a : b;
+}
+int max3(int a, int b, int c) {
+    return max2(max2(a, b), c);
 }
 
-int main(int argc, char const *argv[])
-{
+int maxCrossingSum(int * t, int head, int med, int tail) {
+    int sum = 0;
+    int left_sum = INT_MIN;
+    for (int i = med; i >= head; --i) {
+        sum = sum + t[i];
+        if (sum > left_sum)
+          left_sum = sum;
+    }
+    sum = 0;
+    int right_sum = INT_MIN;
+    for (int i = med + 1; i <= tail; ++i) {
+        sum = sum + t[i];
+        if (sum > right_sum)
+          right_sum = sum;
+    }
+    return left_sum + right_sum;
+}
+
+int maxSubArraySum(int * t, int head, int tail) {
+    if (head == tail)
+        return t[head];
+    int med = (head + tail) / 2;
+    return max3(maxSubArraySum(t, head, med),
+                maxSubArraySum(t, med + 1, tail),
+                maxCrossingSum(t, head, med, tail));
+}
+
+int main(int argc, char const *argv[]) {
     int tailleT = argc-1;
     int * t = loadTab( tailleT, argv);
 
-    int t1_start, t1_end, t2_start, t2_end, t3_start, t3_end;
+    int somme_max, start_max, end_max;
 
-    int r1 = seqSommeMax(t, 0, tailleT/2, &t1_start, &t1_end);
-    int r2 = seqSommeMax(t, tailleT/2, tailleT, &t2_start, &t2_end);
-    int r3 = seqSommeMax(t, 0+tailleT/2, tailleT-tailleT/2, &t3_start, &t3_end);
-
-    if(r1 > r2 && r1 > r3) {
-        printf("%d[%d:%d]\n", r1, t1_start, t1_end);
-    } else if (r2 > r3) {
-        printf("%d[%d:%d]\n", r2, t2_start, t2_end);
-    } else
-        printf("%d[%d:%d]\n", r3, t3_start, t3_end);
+    somme_max = maxSubArraySum(t, 0, tailleT - 1);
 
     free(t);
+
+    printf("%d[%d:%d]\n", somme_max, start_max, end_max);
 
     return 0;
 }

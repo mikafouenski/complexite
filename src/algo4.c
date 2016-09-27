d114cac53bf7ea98d83708adc54ed16d67c2175c
Mickaël Bernardini <contact@mickael-bernardini.fr>
2016-09-27 08:48:45 +0200

    Fix algo 4

--- src/algo4.c
+++ src/algo4.c
@@ -1,21 +1,25 @@
 #include "algo4.h"
 
 int algo4 (int * t, int n, int *start_max, int *end_max) {
-    int start = 0, somme = 0, sommeMax = t[0];
+    int sum_max = 0, sum = 0, i = 0;
     *start_max = 0;
-    *end_max = 0;
-    for(int i = 0; i < n; ++i) {
-        if(somme + t[i] >= 0)
-            somme += t[i];
-        else {
-            somme = t[++i];
-            start = i;
-        }
-        if(sommeMax < somme) {
-            sommeMax = somme;
-            *start_max = start;
-            *end_max = i;
+    *end_max = -1;
+    for (int j = 0; j < n; j++) {
+        sum += t[j];
+        if (sum < 0) {
+            i = j + 1;
+            sum = 0;
+        } else if (sum > sum_max) {
+            sum_max = sum;
+            *start_max = i;
+            *end_max   = j;
         }
     }
-    return sommeMax;
+    if (*start_max <= *end_max && *start_max >= 0 && *end_max >= 0) {
+        return sum_max;
+    } else {
+        *start_max = 0;
+        *end_max = 0;
+        return 0;
+    }
 }
